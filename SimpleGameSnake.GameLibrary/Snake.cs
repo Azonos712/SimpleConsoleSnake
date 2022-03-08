d0aaa8cb7ec2a1d4361ad394496f173724c0605f
Azonos712 <nekoooolay@mail.ru>
2022-03-08 17:39:27 +0300

    Update snake model

--- SimpleGameSnake.GameLibrary/Snake.cs
+++ SimpleGameSnake.GameLibrary/Snake.cs
@@ -8,15 +8,31 @@ namespace SimpleGameSnake.GameLibrary
 {
     public class Snake
     {
-        public int Length { get; private set; }
+        public Point Head { get; private set; }
+        public Point PrevHead { get; private set; }
+        public Point LastPart { get; private set; }
+        public int TailLength { get; private set; }
         public Direction CurrentDirection { get; private set; }
+
+        public Snake(int headX, int headY)
+        {
+            Head.X = headX;
+            Head.Y = headY;
+            TailLength = 2;
+            LastPart.X = headX - TailLength;
+            LastPart.Y = Head.Y;
+            CurrentDirection = Direction.Right;
+        }
+
         public void Eat(Food food)
         {
 
         }
+
         public void ChangeDirection(Direction newDirection)
         {
-
+            //TODO Проверка на противоположную сторону
+            CurrentDirection = newDirection;
         }
     }
 }
