5591d202e95993f675e973d4c8ffa12e97ae0867
Azonos712 <nekoooolay@mail.ru>
2022-03-10 21:13:02 +0300

    Separated the logic for displaying graphics in the console into a class

---
+++ SimpleGameSnake.ConsoleUI/ConsoleGraphics.cs
@@ -0,0 +1,88 @@
+﻿using SimpleGameSnake.GameLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SimpleGameSnake.ConsoleUI
+{
+    public class ConsoleGraphics
+    {
+        public readonly char VerticalWallSymbol = '│';
+        public readonly char HorizontalWallSymbol = '─';
+        public readonly char SnakeHeadUpSymbol = '^';
+        public readonly char SnakeHeadDownSymbol = 'v';
+        public readonly char SnakeHeadLeftSymbol = '<';
+        public readonly char SnakeHeadRightSymbol = '>';
+        public readonly char SnakeBodySymbol = 'o';
+        public readonly char FoodSymbol = '@';
+
+        public void ShowField(int width, int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0 || i == height - 1)
+                        DisplaySymbol(j, i, HorizontalWallSymbol);
+
+                    if (j == 0 || j == width - 1)
+                        DisplaySymbol(j, i, VerticalWallSymbol);
+                }
+            }
+        }
+
+        private void DisplaySymbol(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+
+        public void ShowSnake(Snake snake)
+        {
+            DisplaySymbol(snake.Head.X, snake.Head.Y, GetHeadSymbolByDirection(snake.CurrentDirection));
+
+            for (int i = 1; i <= snake.TailLength; i++)
+                DisplaySymbol(snake.Head.X - i, snake.Head.Y, SnakeBodySymbol);
+        }
+
+        private char GetHeadSymbolByDirection(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => SnakeHeadUpSymbol,
+                Direction.Down => SnakeHeadDownSymbol,
+                Direction.Left => SnakeHeadLeftSymbol,
+                Direction.Right => SnakeHeadRightSymbol,
+                _ => throw new Exception(),
+            };
+        }
+
+        public void ShowFood(int x, int y)
+        {
+            DisplaySymbol(x, y, FoodSymbol);
+        }
+
+        public void ShowSnakeStep(Snake snake, GameManager game)
+        {
+            if (game.IsGameOver)
+            {
+                DisplaySymbol(snake.PrevHead.X, snake.PrevHead.Y, GetHeadSymbolByDirection(snake.CurrentDirection));
+                return;
+            }
+
+            DisplaySymbol(snake.Head.X, snake.Head.Y, GetHeadSymbolByDirection(snake.CurrentDirection));
+            DisplaySymbol(snake.PrevHead.X, snake.PrevHead.Y, SnakeBodySymbol);
+            if (game.IsFoodOnField)
+                DisplaySymbol(snake.LastPart.X, snake.LastPart.Y, ' ');
+        }
+
+        public void ShowScoreUnderTheField(int score, int width, int height)
+        {
+            Console.SetCursorPosition(3, height + 1);
+            Console.WriteLine($"Score - {score}");
+        }
+    }
+}
--- SimpleGameSnake.ConsoleUI/Program.cs
+++ SimpleGameSnake.ConsoleUI/Program.cs
@@ -9,80 +9,44 @@ namespace SimpleGameSnake.ConsoleUI
     {
         private const int UPDATEPERSECONDS = 100;
 
-        private const char VERTICAL_WALL_SYMBOL = '│';
-        private const char HORIZONTAL_WALL_SYMBOL = '─';
-        private const char SNAKE_HEAD_UP_SYMBOL = '^';
-        private const char SNAKE_HEAD_DOWN_SYMBOL = 'v';
-        private const char SNAKE_HEAD_LEFT_SYMBOL = '<';
-        private const char SNAKE_HEAD_RIGHT_SYMBOL = '>';
-        private const char SNAKE_BODY_SYMBOL = 'o';
-        private const char FOOD_SYMBOL = '@';
-
         private static GameManager _game;
 
         static async Task Main(string[] args)
         {
             ConsoleSettings settings = new ConsoleSettings();
+            ConsoleGraphics graphics = new ConsoleGraphics();
             Snake snake = new Snake(settings.FieldWidth / 2, settings.FieldHeight / 2);
             _game = new GameManager();
 
             Console.WriteLine("Press any key to start...");
             Console.ReadKey();
 
-            StartGame(settings, snake);
-            await MainLoop(settings, snake);
+            StartGame(settings, graphics, snake);
+            await MainLoop(settings, graphics, snake);
 
             Console.WriteLine("Game Over");
             Console.ReadKey();
         }
 
-        private static void StartGame(ConsoleSettings settings, Snake snake)
+        private static void StartGame(ConsoleSettings settings, ConsoleGraphics graphics, Snake snake)
         {
             Console.Clear();
-            ShowField(settings.FieldWidth, settings.FieldHeight);
-            ShowSnake(snake);
+            graphics.ShowField(settings.FieldWidth, settings.FieldHeight);
+            graphics.ShowSnake(snake);
             Task.Run(() => PressedKeyListener.Instance.ListenKeys(_game, snake));
         }
 
-        private static void ShowField(int width, int height)
-        {
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    if (i == 0 || i == height - 1)
-                        DisplaySymbol(j, i, HORIZONTAL_WALL_SYMBOL);
-
-                    if (j == 0 || j == width - 1)
-                        DisplaySymbol(j, i, VERTICAL_WALL_SYMBOL);
-                }
-            }
-        }
-
-        private static void DisplaySymbol(int x, int y, char symbol)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symbol);
-        }
-
-        private static void ShowSnake(Snake snake)
-        {
-            DisplaySymbol(snake.Head.X, snake.Head.Y, GetHeadSymbolByDirection(snake.CurrentDirection));
-
-            for (int i = 1; i <= snake.TailLength; i++)
-                DisplaySymbol(snake.Head.X - i, snake.Head.Y, SNAKE_BODY_SYMBOL);
-        }
-
-        private static async Task MainLoop(ConsoleSettings settings, Snake snake)
+        private static async Task MainLoop(ConsoleSettings settings, ConsoleGraphics graphics, Snake snake)
         {
             while (!_game.IsGameOver)
             {
                 if (!_game.IsFoodOnField)
                     GenerateFoodOnField(settings.FieldWidth, settings.FieldHeight);
 
-                DisplaySymbol(_game.Food.Position.X, _game.Food.Position.Y, FOOD_SYMBOL);
+                graphics.ShowFood(_game.Food.Position.X, _game.Food.Position.Y);
                 MoveSnake(snake, settings.FieldWidth, settings.FieldHeight);
-                ShowScoreUnderTheField(settings.FieldWidth, settings.FieldHeight);
+                graphics.ShowSnakeStep(snake, _game);
+                graphics.ShowScoreUnderTheField(_game.Score, settings.FieldWidth, settings.FieldHeight);
 
                 await Task.Delay(UPDATEPERSECONDS);
             }
@@ -93,18 +57,6 @@ namespace SimpleGameSnake.ConsoleUI
             _game.CreateFood(1, width - 1, 1, height - 1);
         }
 
-        private static char GetHeadSymbolByDirection(Direction direction)
-        {
-            return direction switch
-            {
-                Direction.Up => SNAKE_HEAD_UP_SYMBOL,
-                Direction.Down => SNAKE_HEAD_DOWN_SYMBOL,
-                Direction.Left => SNAKE_HEAD_LEFT_SYMBOL,
-                Direction.Right => SNAKE_HEAD_RIGHT_SYMBOL,
-                _ => throw new Exception(),
-            };
-        }
-
         private static void MoveSnake(Snake snake, int width, int height)
         {
             snake.MoveSnake();
@@ -113,18 +65,7 @@ namespace SimpleGameSnake.ConsoleUI
 
             SelfBodyCheck(snake);
 
-            var foodHasBeenEaten = FoodCheck(snake);
-
-            if (_game.IsGameOver)
-            {
-                DisplaySymbol(snake.PrevHead.X, snake.PrevHead.Y, GetHeadSymbolByDirection(snake.CurrentDirection));
-                return;
-            }
-
-            DisplaySymbol(snake.Head.X, snake.Head.Y, GetHeadSymbolByDirection(snake.CurrentDirection));
-            DisplaySymbol(snake.PrevHead.X, snake.PrevHead.Y, SNAKE_BODY_SYMBOL);
-            if (!foodHasBeenEaten)
-                DisplaySymbol(snake.LastPart.X, snake.LastPart.Y, ' ');
+            FoodCheck(snake);
         }
 
         private static void ScreenEdgeCheck(Snake snake, int width, int height)
@@ -142,23 +83,13 @@ namespace SimpleGameSnake.ConsoleUI
                 _game.StopGame();
         }
 
-        private static bool FoodCheck(Snake snake)
+        private static void FoodCheck(Snake snake)
         {
             if (snake.Head.X == _game.Food.Position.X && snake.Head.Y == _game.Food.Position.Y)
             {
                 _game.PutAwayFood();
                 snake.Eat();
-
-                return true;
             }
-
-            return false;
-        }
-
-        private static void ShowScoreUnderTheField(int width, int height)
-        {
-            Console.SetCursorPosition(3, height + 1);
-            Console.WriteLine($"Score - {_game.Score}");
         }
     }
 }
