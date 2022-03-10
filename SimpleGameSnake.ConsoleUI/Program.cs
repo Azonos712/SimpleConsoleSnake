4b3e3747373869888d1f65a3e8c30b6ba66efc38
Azonos712 <nekoooolay@mail.ru>
2022-03-10 21:34:19 +0300

    Separated all the game logic into the game manager class

--- SimpleGameSnake.ConsoleUI/Program.cs
+++ SimpleGameSnake.ConsoleUI/Program.cs
@@ -1,93 +1,47 @@
 ﻿using SimpleGameSnake.GameLibrary;
-using System;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace SimpleGameSnake.ConsoleUI
 {
     internal class Program
     {
-
-        private static GameManager _game;
-
         static async Task Main(string[] args)
         {
             ConsoleSettings settings = new ConsoleSettings();
             ConsoleGraphics graphics = new ConsoleGraphics();
             Snake snake = new Snake(settings.FieldWidth / 2, settings.FieldHeight / 2);
-            _game = new GameManager();
+            GameManager game = new GameManager();
 
             Console.WriteLine("Press any key to start...");
             Console.ReadKey();
 
-            StartGame(settings, graphics, snake);
-            await MainLoop(settings, graphics, snake);
+            StartGame(game, settings, graphics, snake);
+            await MainLoop(game, settings, graphics, snake);
 
             Console.WriteLine("Game Over");
             Console.ReadKey();
         }
 
-        private static void StartGame(ConsoleSettings settings, ConsoleGraphics graphics, Snake snake)
+        private static void StartGame(GameManager game, ConsoleSettings settings, ConsoleGraphics graphics, Snake snake)
         {
             Console.Clear();
             graphics.DisplayField(settings.FieldWidth, settings.FieldHeight);
             graphics.DisplaySnake(snake);
-            Task.Run(() => PressedKeyListener.Instance.ListenKeys(_game, snake));
+            Task.Run(() => PressedKeyListener.Instance.ListenKeys(game, snake));
         }
 
-        private static async Task MainLoop(ConsoleSettings settings, ConsoleGraphics graphics, Snake snake)
+        private static async Task MainLoop(GameManager game, ConsoleSettings settings, ConsoleGraphics graphics, Snake snake)
         {
-            while (!_game.IsGameOver)
+            while (!game.IsGameOver)
             {
-                if (!_game.IsFoodOnField)
-                    GenerateFoodOnField(settings.FieldWidth, settings.FieldHeight);
+                if (!game.IsFoodOnField)
+                    game.GenerateFoodOnField(1, settings.FieldWidth - 1, 1, settings.FieldHeight - 1);
 
-                graphics.DisplayFood(_game.Food.Position.X, _game.Food.Position.Y);
-                MoveSnake(snake, settings.FieldWidth, settings.FieldHeight);
-                graphics.DisplaySnakeStep(snake, _game);
-                graphics.DisplayScoreUnderTheField(_game.Score, settings.FieldWidth, settings.FieldHeight);
+                graphics.DisplayFood(game.Food.Position.X, game.Food.Position.Y);
+                game.MoveSnake(snake, settings.FieldWidth, settings.FieldHeight);
+                graphics.DisplaySnakeStep(snake, game);
+                graphics.DisplayScoreUnderTheField(game.Score, settings.FieldWidth, settings.FieldHeight);
 
                 await Task.Delay(settings.ConsoleRefreshDelay);
-            }
-        }
-
-        private static void GenerateFoodOnField(int width, int height)
-        {
-            _game.CreateFood(1, width - 1, 1, height - 1);
-        }
-
-        private static void MoveSnake(Snake snake, int width, int height)
-        {
-            snake.MoveSnake();
-
-            ScreenEdgeCheck(snake, width, height);
-
-            SelfBodyCheck(snake);
-
-            FoodCheck(snake);
-        }
-
-        private static void ScreenEdgeCheck(Snake snake, int width, int height)
-        {
-            if (snake.Head.X == 0 || snake.Head.X == width - 1)
-                _game.StopGame();
-
-            if (snake.Head.Y == 0 || snake.Head.Y == height - 1)
-                _game.StopGame();
-        }
-
-        private static void SelfBodyCheck(Snake snake)
-        {
-            if (snake.Body.Contains(snake.Head))
-                _game.StopGame();
-        }
-
-        private static void FoodCheck(Snake snake)
-        {
-            if (snake.Head.X == _game.Food.Position.X && snake.Head.Y == _game.Food.Position.Y)
-            {
-                _game.PutAwayFood();
-                snake.Eat();
             }
         }
     }
---
+++ SimpleGameSnake.GameLibrary/GameManager.cs
@@ -0,0 +1,69 @@
+﻿namespace SimpleGameSnake.GameLibrary
+{
+    public class GameManager
+    {
+        public bool IsGameOver { get; private set; }
+        public bool IsFoodOnField { get; private set; }
+        public int Score { get; private set; }
+        public Food Food { get; private set; }
+
+        public GameManager()
+        {
+            IsGameOver = false;
+            Score = 0;
+            Food = new Food(1);
+        }
+
+        public void MoveSnake(Snake snake, int width, int height)
+        {
+            snake.MoveSnake();
+
+            ScreenEdgeCheck(snake, width, height);
+
+            SelfBodyCheck(snake);
+
+            FoodCheck(snake);
+        }
+
+        private void ScreenEdgeCheck(Snake snake, int width, int height)
+        {
+            if (snake.Head.X == 0 || snake.Head.X == width - 1)
+                StopGame();
+
+            if (snake.Head.Y == 0 || snake.Head.Y == height - 1)
+                StopGame();
+        }
+
+        private void StopGame()
+        {
+            IsGameOver = true;
+        }
+
+        private void SelfBodyCheck(Snake snake)
+        {
+            if (snake.Body.Contains(snake.Head))
+                StopGame();
+        }
+
+        private void FoodCheck(Snake snake)
+        {
+            if (snake.Head.X == Food.Position.X && snake.Head.Y == Food.Position.Y)
+            {
+                PutAwayFoodFromField();
+                snake.Eat();
+            }
+        }
+
+        private void PutAwayFoodFromField()
+        {
+            IsFoodOnField = false;
+            Score += Food.Value;
+        }
+
+        public void GenerateFoodOnField(int minX, int maxX, int minY, int maxY)
+        {
+            Food.CreateNewFood(minX, maxX, minY, maxY);
+            IsFoodOnField = true;
+        }
+    }
+}
